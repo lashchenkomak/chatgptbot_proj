ffd9088a7da364abeb1a274c0d753a282219e4b2
lashchenkomak <maksym.lashchenko06@gmail.com>
2024-12-02 14:10:09 +0200

    Add custom logging

--- main.py
+++ main.py
@@ -15,19 +15,30 @@ my_bot_key = os.getenv('BOT_KEY')
 bot = telebot.TeleBot(my_bot_key)
 openai.api_key = my_api_key
 
-logging.basicConfig(filename='bot.log', level=logging.INFO, format='%(asctime)s - %(message)s')
+logger = logging.getLogger('ChatGPT_bot')
+logger.setLevel(logging.INFO)
+
+file_handler = logging.FileHandler('bot.log')
+file_handler.setLevel(logging.INFO)
+
+console_handler = logging.StreamHandler()
+console_handler.setLevel(logging.INFO)
+
+custom_des = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+file_handler.setFormatter(custom_des)
+console_handler.setFormatter(custom_des)
+
+logger.addHandler(file_handler)
+logger.addHandler(console_handler)
+
 
 user_data = {}
-
-load_dotenv()
-
 
 @bot.message_handler(commands=['start'])
 def start_command(message):
     user_data[message.chat.id] = {'user_model': '', 'user_role': '', 'user_history': []}
     bot.send_message(message.chat.id, WELCOME_MESSAGE, reply_markup=create_role_buttons())
-    logging.info(f'User {message.chat.id} started the bot.')
-
+    logger.info(f'User {message.chat.id} started the bot.')
 
 @bot.callback_query_handler(func=lambda call: True)
 def handle_selection(call):
@@ -39,7 +50,7 @@ def handle_selection(call):
 
     if call.data in ROLE_MESSAGES:
         user_info['user_role'] = call.data
-        logging.info(f' User {chat_id} selected role: {call.data}')
+        logger.info(f'User {chat_id} selected role: {call.data}')
 
         time.sleep(1)
         bot.send_message(chat_id, ROLE_MESSAGES[call.data])
@@ -50,17 +61,10 @@ def handle_selection(call):
         user_info['role_message_id'] = role_message.message_id
     elif call.data in GPT_MODELS:
         user_info['user_model'] = call.data
-        logging.info(f'User {chat_id} selected model: {call.data}')
-
-        if 'role_message_id' in user_info:
-            try:
-                bot.delete_message(chat_id, user_info['role_message_id'])
-            except Exception as e:
-                logging.warning(f'Something bad:{e}')
+        logger.info(f'User {chat_id} selected model: {call.data}')
 
         time.sleep(1)
         bot.send_message(chat_id, MODEL_SELECTED_MESSAGES[call.data])
-
 
 @bot.message_handler(content_types=['photo'])
 def handle_photo(message):
@@ -69,13 +73,12 @@ def handle_photo(message):
 
     if not user_info or not user_info['user_model']:
         bot.send_message(chat_id, 'Please select a role and model first.')
-        logging.warning(f'User {chat_id} The user as stupid as brick and tried to send a photo without selecting role '
-                        f'or model.')
+        logger.warning(f'User {chat_id} attempted to send a photo without selecting a role or model.')
         return
 
     time.sleep(1)
     bot.send_message(chat_id, 'Photo received, processing... ⏳')
-    logging.info(f'User {chat_id} sent a photo.')
+    logger.info(f'User {chat_id} sent a photo.')
 
     file_info = bot.get_file(message.photo[-1].file_id)
     downloaded_file = bot.download_file(file_info.file_path)
@@ -85,7 +88,7 @@ def handle_photo(message):
 
     extracted_text = extract_text_from_image('user_photo.jpg')
     user_info['user_history'].append({'role': 'user', 'content': extracted_text})
-    logging.info(f'From photo: {extracted_text}')
+    logger.info(f'Extracted text from photo: {extracted_text}')
 
     response = openai.ChatCompletion.create(
         model=user_info['user_model'],
@@ -97,8 +100,7 @@ def handle_photo(message):
 
     time.sleep(1)
     bot.reply_to(message, bot_reply)
-    logging.info(f'User {chat_id}: {bot_reply}')
-
+    logger.info(f'Response to user {chat_id}: {bot_reply}')
 
 @bot.message_handler(func=lambda message: True)
 def handle_text(message):
@@ -107,13 +109,12 @@ def handle_text(message):
 
     if not user_info or not user_info['user_model']:
         bot.send_message(chat_id, 'Please select a role and model first.')
-        logging.warning(f'User {chat_id} The user as stupid as brick and tried to send a message without selecting role'
-                        f' or model.')
-        return None
+        logger.warning(f'User {chat_id} attempted to send a message without selecting a role or model.')
+        return
 
     time.sleep(1)
-    bot.send_message(chat_id, 'The message received, processing... ⏳')
-    logging.info(f'User {chat_id}: {message.text}')
+    bot.send_message(chat_id, 'Message received, processing... ⏳')
+    logger.info(f'User {chat_id} sent a message: {message.text}')
     user_info['user_history'].append({'role': 'user', 'content': message.text})
 
     response = openai.ChatCompletion.create(
@@ -126,7 +127,6 @@ def handle_text(message):
 
     time.sleep(1)
     bot.reply_to(message, bot_reply)
-    logging.info(f'User {chat_id}: {bot_reply}')
-
+    logger.info(f'Response to user {chat_id}: {bot_reply}')
 
 bot.polling(none_stop=True)
